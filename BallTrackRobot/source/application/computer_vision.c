1a10f25f7347eac5df559267626ff832e484b5d2
sehshf <sehshf@gmail.com>
2017-02-07 22:59:26 +0100

    "test_ball_detect.c" compiled.

--- BallTrackRobot/source/application/computer_vision.c
+++ BallTrackRobot/source/application/computer_vision.c
@@ -26,7 +26,7 @@
  * FILE SCOPE VARIABLES (static)					*
  * **************************************************
  */
-static int8_T 	servoPos[NUM_MOTORS];	// Can vary from -90 to 90
+
 
 
 /*
@@ -57,91 +57,17 @@ static int8_T 	servoPos[NUM_MOTORS];	// Can vary from -90 to 90
 *  		   Oct 2016
 *  -------------------------------------------------------  *
 */
-void InitServos(void)
-{
-	SetupPCABoard();
-	SetPCAFreq(50);		// 50 Hz
-	DriveServoAbs(PAN_MOTOR  , 0);
-	DriveServoAbs(TILT_MOTOR, 0);
-
-} // END: InitServos()
-
-
-/**
-*  -------------------------------------------------------  *
-*  FUNCTION:
-*      DRIVESERVOABS()
-*      Drive the servo motors to an absolute position.
-*
-*  Inputs:
-*      motor : Specifies the servo motor.
-*      degree: Absolute position in degree -90 to 90
-*
-*  Outputs:
-*
-*  Author: Ehsan Shafiei
-*  		   Oct 2016
-*  -------------------------------------------------------  *
-*/
-void DriveServoAbs(uint8_T motor, int8_T degree)
-{
-	uint16_T pulse;
-
-	pulse = SERVO_PULSE_NEUT + degree * (SERVO_PULSE_MAX - SERVO_PULSE_MIN) / SERVO_POS_MAX / 2;	// pulse [ms]
-
-	servoPos[motor] = degree;		// Update position
-
-	SetPCAPWM(motor, pulse);
-
-} // END: DriveServoAbs()
-
-/**
-*  -------------------------------------------------------  *
-*  FUNCTION:
-*      DRIVESERVOINC()
-*      Drive the servo motors with an incremental position degree.
-*
-*  Inputs:
-*      motor 	: Specifies the servo motor.
-*      direction: Rotation direction
-*      degree	: Absolute position in degree -90 to 90
-*
-*  Outputs:
-*
-*  Author: Ehsan Shafiei
-*  		   Oct 2016
-*  -------------------------------------------------------  *
-*/
-void DriveServoInc(uint8_T motor, int8_T direction, int8_T degree)
-{
-    servoPos[motor] = servoPos[motor] + direction * degree;
-    servoPos[motor] = min(SERVO_POS_MAX, max(-SERVO_POS_MAX, servoPos[motor]));
-    DriveServoAbs(motor, servoPos[motor]);
-
-} // END: DriveServoInc()
+//void InitServos(void)
+//{
+//	SetupPCABoard();
+//	SetPCAFreq(50);		// 50 Hz
+//	DriveServoAbs(PAN_MOTOR  , 0);
+//	DriveServoAbs(TILT_MOTOR, 0);
+//
+//} // END: InitServos()
 
 
-/**
-*  -------------------------------------------------------  *
-*  FUNCTION:
-*      GETSERVOPOS()
-*      Return the absolute position of the specified servo motor.
-*
-*  Inputs:
-*      motor : Specifies the servo motor.
-*
-*  Outputs:
-*      servoPos : Position.
-*
-*  Author: Ehsan Shafiei
-*  		   Oct 2016
-*  -------------------------------------------------------  *
-*/
-int8_T GetServoPos(uint8_T motor)
-{
-	return servoPos[motor];
 
-} // END: GetServoPos()
 
 
 /*
--- BallTrackRobot/source/application/test/test_ball_detect.c
+++ BallTrackRobot/source/application/test/test_ball_detect.c
@@ -11,6 +11,10 @@
  * **************************************************
  */
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 /*
  * **************************************************
@@ -24,29 +28,114 @@
 
 int main(void)
 {
-	uint8_T motor;
-	int16_T  degree;
-	int16_T direction;
+	int H_MIN = 30;
+	int H_MAX = 64;
+	int S_MIN = 86;
+	int S_MAX = 255;
+	int V_MIN = 34;
+	int V_MAX = 255;
 
-	InitServos();
+	int numCont;
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	CvSeq* firstCont = NULL;
+	CvMoments moment;
+
+	int x = 0, y = 0;
+
+	//minimum and maximum object area
+	const int AREA_MIN = 20 * 20;
+	const int AREA_MAX = 640 * 480 * 3 / 4;
+
+	int area, lastArea;
+	boolean_T dtcdFlag;
+
+	printf("Start video capturing\n");
+	CvCapture* capture =  cvCreateCameraCapture(0);
+
+	cvNamedWindow("piWindow", CV_WINDOW_AUTOSIZE);
+	cvMoveWindow("piWindow", 400, 200);
+
+//	cvNamedWindow("piWindow2", CV_WINDOW_AUTOSIZE);
+//	cvMoveWindow("piWindow2", 1100, 200);
+
+//	cvNamedWindow("cnt", CV_WINDOW_AUTOSIZE);
+//	cvMoveWindow("piWindow2", 1100, 400);
+
+	IplImage* img;
+	IplImage* img2 = cvCreateImage(CvSize(640, 480), IPL_DEPTH_8U, 3);
+	IplImage* imgTmp = cvCreateImage(CvSize(640, 480), IPL_DEPTH_8U, 1);
+	IplImage* imgFild = cvCreateImage(CvSize(640, 480), IPL_DEPTH_8U, 1);
+
+	cvCreateTrackbar("H1","cnt",&H_MIN,255,0);
+	cvCreateTrackbar("H2","cnt",&H_MAX,255,0);
+	cvCreateTrackbar("S1","cnt",&S_MIN,255,0);
+	cvCreateTrackbar("S2","cnt",&S_MAX,255,0);
+	cvCreateTrackbar("V1","cnt",&V_MIN,255,0);
+	cvCreateTrackbar("V2","cnt",&V_MAX,255,0);
+
+	IplConvKernel *erodeElement, *dilateElement;
+
+	erodeElement  = cvCreateStructuringElementEx(3, 3, 0, 0, CV_SHAPE_RECT);;
+	dilateElement = cvCreateStructuringElementEx(8, 8, 0, 0, CV_SHAPE_RECT);
 
 	while(1)
-    {
+	{
 
-		printf("Enter motor\n");
-		scanf("%hhu", &motor);
+		lastArea  = 0;
 
-		printf("Enter degree:\n");
-		scanf("%hd", &degree);
+		img = cvQueryFrame(capture);
 
-		//DriveServoAbs(motor, (int8_T)degree);
+		cvCvtColor(img, img2, CV_BGR2HSV);
 
-		printf("Enter direction:\n");
-		scanf("%hd", &direction);
-		DriveServoInc(motor, (int8_T)direction, (int8_T)degree);
-    }
+		cvInRangeS(img2, cvScalar(H_MIN, S_MIN, V_MIN), cvScalar(H_MAX, S_MAX, V_MAX), imgTmp);
 
-    return PASSED;
+		cvErode (imgTmp, imgTmp, erodeElement , 2);
+		cvDilate(imgTmp, imgTmp, dilateElement, 2);
+
+		cvCopy(imgTmp, imgFild, NULL);
+
+		numCont = cvFindContours(imgTmp, storage, &firstCont);
+
+		if (numCont > 0)
+		{
+			for (; firstCont != NULL; firstCont = firstCont->h_next)
+			{
+				cvMoments(firstCont, &moment, 0);
+				area = moment.m00;
+
+				if (area > AREA_MIN && area < AREA_MAX && area > lastArea)
+				{
+					x = (int)(moment.m10 / moment.m00);
+					y = (int)(moment.m01 / moment.m00);
+
+					lastArea  = area;
+					dtcdFlag = TRUE;
+				}
+				else
+				{
+					dtcdFlag = FALSE;
+				}
+			}
+			if (dtcdFlag)
+				cvCircle(img, CvPoint(x, y), 5, CV_RGB(255, 0, 0));
+		}
+
+		cvShowImage("piWindow" , img );
+		//cvShowImage("piWindow2", imgFild);
+
+		// wait for a key
+		cvWaitKey(30);
+	}
+
+//		printf("Channel = %d, Size = %d, width = %d, height = %d, Depth = %d\n",
+//				imgBall->nChannels, imgBall->nSize, imgBall->width, imgBall->height, imgBall->depth);
+
+	cvReleaseStructuringElement(&erodeElement );
+	cvReleaseStructuringElement(&dilateElement);
+
+	cvReleaseCapture(&capture);
+
+	return PASSED;
 
 } // END: main()
 
